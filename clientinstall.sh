b2074b407a15ce049f34ab49db007ee0e90a4b5b
dev-ajay-git <er.ajay88@gmail.com>
2025-06-28 11:30:25 +0530

    Update clientinstall.sh

--- clientinstall.sh
+++ clientinstall.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-yum -y install openldap-clients nss-pam-ldapd
+# Update package list and install required packages
+sudo apt update
+sudo apt install -y libnss-ldap libpam-ldap ldap-utils nslcd
+
+# Ask for LDAP server IP
 read -p 'Enter LDAP Server IP Address: ' ip
-authconfig --enableldap --enableldapauth --ldapserver=$ip --ldapbasedn="dc=linuxautomations,dc=com" --enablemkhomedir --update
+
+# Configure LDAP client
+sudo auth-client-config -t nss -p lac_ldap
+
+# Modify /etc/ldap.conf or /etc/nslcd.conf to point to your LDAP server IP
+sudo sed -i "s|^uri .*|uri ldap://$ip/|" /etc/ldap/ldap.conf
+
+# Configure NSS to use LDAP
+sudo pam-auth-update --enable mkhomedir
+
+echo "LDAP client setup complete."
